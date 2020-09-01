1639ca1a1ea7ca09e03e1459d1290401db06628d
王水胜 <602466011@qq.com>
2020-09-01 10:08:59 +0800

    更新: 使用箭头函数代替$.proxy

--- js/button.js
+++ js/button.js
@@ -36,7 +36,7 @@
     if (data.resetText == null) $el.data('resetText', $el[val]())
 
     // push to event loop to allow forms to submit
-    setTimeout($.proxy(function () {
+    /* setTimeout($.proxy(function () {
       $el[val](data[state] == null ? this.options[state] : data[state])
 
       if (state == 'loadingText') {
@@ -46,7 +46,20 @@
         this.isLoading = false
         $el.removeClass(d).removeAttr(d).prop(d, false)
       }
-    }, this), 0)
+    }, this), 0) */
+
+    // 使用箭头函数代替 $.proxy
+    setTimeout(() => {
+      $el[val](data[state] == null ? this.options[state] : data[state])
+
+      if (state == 'loadingText') {
+        this.isLoading = true
+        $el.addClass(d).attr(d, d).prop(d, true)
+      } else if (this.isLoading) {
+        this.isLoading = false
+        $el.removeClass(d).removeAttr(d).prop(d, false)
+      }
+    }, 0)
   }
 
   Button.prototype.toggle = function () {
